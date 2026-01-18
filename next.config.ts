b8db2947a3e3f56949e1c9d390de35a11613a66b
Anshika1201 <anshika12012006@gmail.com>
2026-01-18 22:02:13 +0530

    edit next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: false, // keep strict
+  },
   images: {
     domains: ["localhost"],
   },
